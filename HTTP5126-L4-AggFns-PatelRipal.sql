8aad3734cb84aa43f32e7210851cbf020237722d
RipalPatel18 <n01578468@humber.ca>
2025-07-29 16:57:31 -0400

    Initial commit

--- HTTP5126-L4-AggFns-PatelRipal.sql
+++ HTTP5126-L4-AggFns-PatelRipal.sql
@@ -2,39 +2,39 @@
 -- Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 6; your query for question 1B should go on line 8...
 
 --  1 
--- A 
+ A 
 SELECT MIN(price) FROM stock_item;
 
--- B 
+B 
 SELECT MAX(price) FROM stock_item;
 
--- C 
+ C 
 SELECT AVG(price) FROM stock_item;
 
--- D 
+ D 
 SELECT SUM(price) FROM stock_item;
 
 
 --  2
--- A 
+ A 
 SELECT role, COUNT(*) FROM `employee` GROUP BY role;
 
--- B 
+ B 
 SELECT 'Mammals' AS category, COUNT(*) AS item_count FROM stock_item WHERE category <> 'piscine';
 
--- C 
+ C 
 SELECT category, AVG(price) AS average_price FROM stock_item WHERE inventory > 0 GROUP BY category; 
 
 
 
 --  3
--- A  
+ A  
 SELECT category AS "Species", SUM(inventory) AS "In Stock" FROM stock_item GROUP BY category ORDER BY category;
 
--- B  
+ B  
 SELECT category, SUM(inventory), AVG(price) FROM stock_item GROUP BY category HAVING SUM(inventory) < 100 AND AVG(price) < 100;
 
 
 --  4
--- 
+
 A SELECT CONCAT('$', price * inventory) AS 'Potential Earnings',item AS 'Product', CONCAT('$', price) AS 'Price', inventory AS 'Stock Remaining', category AS 'Species' FROM stock_item ORDER BY price * inventory DESC LIMIT 20;
