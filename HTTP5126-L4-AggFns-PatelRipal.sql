b35582a9d86ada23d676f39a0aef6dabe8c2ae43
RipalPatel18 <n01578468@humber.ca>
2025-07-29 16:54:58 -0400

    Resolved merge conflicts

--- HTTP5126-L4-AggFns-PatelRipal.sql
+++ HTTP5126-L4-AggFns-PatelRipal.sql
@@ -2,29 +2,39 @@
 -- Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 6; your query for question 1B should go on line 8...
 
 --  1 
--- A SELECT MIN(price) FROM stock_item;
+-- A 
+SELECT MIN(price) FROM stock_item;
 
--- B SELECT MAX(price) FROM stock_item;
+-- B 
+SELECT MAX(price) FROM stock_item;
 
--- C SELECT AVG(price) FROM stock_item;
+-- C 
+SELECT AVG(price) FROM stock_item;
 
--- D SELECT SUM(price) FROM stock_item;
+-- D 
+SELECT SUM(price) FROM stock_item;
 
 
 --  2
--- A SELECT role, COUNT(*) FROM `employee` GROUP BY role;
+-- A 
+SELECT role, COUNT(*) FROM `employee` GROUP BY role;
 
--- B SELECT 'Mammals' AS category, COUNT(*) AS item_count FROM stock_item WHERE category <> 'piscine';
+-- B 
+SELECT 'Mammals' AS category, COUNT(*) AS item_count FROM stock_item WHERE category <> 'piscine';
 
--- C SELECT category, AVG(price) AS average_price FROM stock_item WHERE inventory > 0 GROUP BY category; 
+-- C 
+SELECT category, AVG(price) AS average_price FROM stock_item WHERE inventory > 0 GROUP BY category; 
 
 
 
 --  3
--- A  SELECT category AS "Species", SUM(inventory) AS "In Stock" FROM stock_item GROUP BY category ORDER BY category;
+-- A  
+SELECT category AS "Species", SUM(inventory) AS "In Stock" FROM stock_item GROUP BY category ORDER BY category;
 
--- B  SELECT category, SUM(inventory), AVG(price) FROM stock_item GROUP BY category HAVING SUM(inventory) < 100 AND AVG(price) < 100;
+-- B  
+SELECT category, SUM(inventory), AVG(price) FROM stock_item GROUP BY category HAVING SUM(inventory) < 100 AND AVG(price) < 100;
 
 
 --  4
--- A SELECT CONCAT('$', price * inventory) AS 'Potential Earnings',item AS 'Product', CONCAT('$', price) AS 'Price', inventory AS 'Stock Remaining', category AS 'Species' FROM stock_item ORDER BY price * inventory DESC LIMIT 20;
+-- 
+A SELECT CONCAT('$', price * inventory) AS 'Potential Earnings',item AS 'Product', CONCAT('$', price) AS 'Price', inventory AS 'Stock Remaining', category AS 'Species' FROM stock_item ORDER BY price * inventory DESC LIMIT 20;
